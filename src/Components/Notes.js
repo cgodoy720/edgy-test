c31ccc729a9faadd9d08740fdfbf23b6f156e9ee
Gillesedgy <edgygilles@pursuit.org>
2023-02-13 20:50:08 -0500

    Fixed Search bar Functionality...

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -13,35 +13,40 @@ export default function Notes({
   handleSubmit,
   handleTextChange,
   handleCheckChange,
-  handleSearch,
+  // handleSearch,
 }) {
-  const [myNotes, setNotes] = useState([]);
+  const [myNotes, setMyNotes] = useState([]);
   //! Search State - Save user Input
   const [search, setSearch] = useState("");
+  //
+  const [filtered, setFiltered] = useState([]);
 
   useEffect(() => {
     axios
       .get(`${API}/notes`)
-      .then((res) => setNotes(res.data))
+      .then((res) => {
+       setFiltered(res.data); // 
+      setMyNotes(res.data);
+      })
       .catch((error) => console.warn(error));
   }, []);
 
   return (
-    <>
-      <div className="Notes-Container">
-        <SearchBar handleSearch={setSearch} />
-        {/* //TODO: FILTER to search for notes that include user inputs */}
-        {/* {myNotes.filter((note)=>(note.content.toLowerCase().includes(handleSearch)))}  */}
-
-        {myNotes.map((note) => {
-          return <Note key={note.id} note={note} />;
-        })}
-        <CreateNote
-          handleSubmit={handleSubmit}
-          handleCheckChange={handleCheckChange}
-          handleTextChnage={handleTextChange}
-        />
-      </div>{" "}
-    </>
+    <div className="Notes-Container">
+      <SearchBar
+        setFiltered={setFiltered}
+        setSearch={setSearch}
+        setMyNotes={setMyNotes}
+        myNotes={myNotes}
+      />
+      {filtered.map((note) => {
+        return <Note key={note.id} note={note} />;
+      })}
+      <CreateNote
+        handleSubmit={handleSubmit}
+        handleCheckChange={handleCheckChange}
+        handleTextChnage={handleTextChange}
+      />
+    </div>
   );
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,36 +1,25 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import "./Search.css";
 import { MdSearch } from "react-icons/md";
 
-const API = process.env.REACT_APP_API_URL;
-export const SearchBar = ({ handleSearch }) => {
-  const [notes, setNotes] = useState("");
-
-
-   //! DONT MIND THIS SIR DAN LOL, I WAS GONNA MAKE AN EXTRA CALL TO GET THE NOTES THEN FILTER IT AGAIN TO RETURN NOTES 
-//   useEffect(() => {
-//     axios
-//       .get(`${API}/notes`)
-//       .then((res) => setNotes(res.data))
-//       .catch((error) => console.warn(error));
-//   }, []);
-
+export const SearchBar = ({ setSearch, setMyNotes, myNotes, setFiltered }) => {
   //HandleSearch
-  const handleSearching = (e) => {
+  const handleSearch = (e) => {
     e.preventDefault();
-    handleSearch(e.target.value);
-
-    console.log(e.target.value);
+    setSearch(e.target.value);
+    if (e.target.value) {
+      setFiltered(
+        myNotes.filter((note) =>
+          note.content.toLowerCase().includes(e.target.value.toLowerCase()))
+      )
+    } else {
+      setFiltered(myNotes);
+    }
   };
   return (
     <div className="SearchBar">
-      <>
-{/* {notes.filter((note)=> note.content.toLowerCase().includes(handleSearching))} */}
-      </>
       <MdSearch className="Search-Icon" />
       <input
-        onChange={handleSearching}
+        onChange={handleSearch}
         type="search"
         placeholder="Search for notes ..."
       />
