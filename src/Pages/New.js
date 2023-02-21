c495e60decdea5d2025b381e8794943c2250a808
Carlos Godoy <carlosgodoy@pursuit.org>
2023-02-21 12:48:53 -0500

    lowercase js

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -1,5 +1,6 @@
 //! ONLY WAY FOR SITE TO DEPLPY WAS TO COMMENT OUT THIS FILE.. NOT SURE WHY
-import CreateNote from "../Components/CreateNote.js";
+// import CreateNote from "../Components/CreateNote.js";
+import CreateNote from "../Components/CreateNote"
 
 export default function New() {
   return (
